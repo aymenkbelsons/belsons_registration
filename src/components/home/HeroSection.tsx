8eeb5fe3dcc2117e96370f3ba061542a3f17d7cd
loordbeerus <kh.skikda@gmail.com>
2025-09-22 10:36:24 +0100

    fix

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -25,58 +25,60 @@ const HeroSection = () => {
     },
   ]
   return (
-    <Container innerClassName='flex justify-center overflow-clip -mt-20' dir={isRTL ? "rtl" : "ltr"}>
-      <div className='uppercase w-full lg:w-2/3 flex flex-col gap-4 lg:gap-7 items-center'>
-        <span className='font-medium text-sm lg:text-lg border border-white rounded-lg p-2.5 lg:p-4 z-10'>
-          {t('hero_sub')}
-        </span>
-        <h1 className=' font-bold text-center text-3xl lg:text-6xl z-10'>
-          {t('hero_title_1')}
-          <br />
-          {t('hero_title_2')}
-          {/*    <br />
+    <Container dir={isRTL ? "rtl" : "ltr"}>
+      <div className='flex justify-center overflow-hidden -mt-20'>
+        <div className='uppercase w-full lg:w-2/3 flex flex-col gap-4 lg:gap-7 items-center'>
+          <span className='font-medium text-sm lg:text-lg border border-white rounded-lg p-2.5 lg:p-4 z-10'>
+            {t('hero_sub')}
+          </span>
+          <h1 className=' font-bold text-center text-3xl lg:text-6xl z-10'>
+            {t('hero_title_1')}
+            <br />
+            {t('hero_title_2')}
+            {/*    <br />
           {t('hero_title_3')} */}
-        </h1>
-        <div className='flex items-center justify-between w-full lg:w-fit lg:gap-12 z-10'>
-          {list.map((item, i) => (
-            <div key={i} className='flex flex-col items-center justify-center text-center gap-1'>
-              <span className='font-bold'>
-                {item.item_1}
+          </h1>
+          <div className='flex items-center justify-between w-full lg:w-fit lg:gap-12 z-10'>
+            {list.map((item, i) => (
+              <div key={i} className='flex flex-col items-center justify-center text-center gap-1'>
+                <span className='font-bold'>
+                  {item.item_1}
+                </span>
+                <span>
+                  {item.item_2}
+                </span>
+              </div>
+            ))}
+          </div>
+          <div className='bordr p-4 border-white flex items-center gap-4 z-10 -mb-10'>
+            <div className='flex flex-col items-center  bg-[#03112F]/50 py-1 px-4 rounded'>
+              <span className='uppercase text-primary'>
+                {t('hero_tainning')}
               </span>
-              <span>
-                {item.item_2}
+              <span className='uppercase font-extrabold text-lg'>
+                12,000 DA
               </span>
             </div>
-          ))}
-        </div>
-        <div className='bordr p-4 border-white flex items-center gap-4 z-10 -mb-10'>
-          <div className='flex flex-col items-center  bg-[#03112F]/50 py-1 px-4 rounded'>
-            <span className='uppercase text-primary'>
-              {t('hero_tainning')}
-            </span>
-            <span className='uppercase font-extrabold text-lg'>
-              12,000 DA
-            </span>
+            <div className='flex flex-col items-center  bg-[#03112F]/50 py-1 px-4 rounded text-sm lg:text-bae'>
+              <span className='uppercase text-primary'>
+                {t('hero_student')}
+              </span>
+              <span className='uppercase font-extrabold text-lg'>
+                9,000 DA
+              </span>
+            </div>
           </div>
-          <div className='flex flex-col items-center  bg-[#03112F]/50 py-1 px-4 rounded text-sm lg:text-bae'>
-            <span className='uppercase text-primary'>
-              {t('hero_student')}
+          <Link href={"#register"} className='px-6 py-3 rounded-lg bg-primary mt-4 z-10 flex flex-col items-center  hover:opacity-65 duration-300 hover:divide-gray-300 ease-in-out transition-all'>
+            <span className='text-xs'>
+              {t('hero_btn_1')}
             </span>
-            <span className='uppercase font-extrabold text-lg'>
-              9,000 DA
+            <span className='font-extrabold text-lg'>
+              {t('hero_btn_2')}
             </span>
-          </div>
+          </Link>
         </div>
-        <Link href={"#register"} className='px-6 py-3 rounded-lg bg-primary mt-4 z-10 flex flex-col items-center  hover:opacity-65 duration-300 hover:divide-gray-300 ease-in-out transition-all'>
-          <span className='text-xs'>
-            {t('hero_btn_1')}
-          </span>
-          <span className='font-extrabold text-lg'>
-            {t('hero_btn_2')}
-          </span>
-        </Link>
+        <div className='w-[800px] h-[800px] lg:w-[1200px] lg:h-[1000px] bg-primary absolute backdrop-blur-2xl blur-3xl -top-[140%] lg:-top-[110%] rounded-full z-0'></div>
       </div>
-      <div className='w-[800px] h-[800px] lg:w-[1200px] lg:h-[1000px] bg-primary absolute backdrop-blur-2xl blur-3xl -top-[140%] lg:-top-[110%] rounded-full z-0'></div>
     </Container>
   )
 }
